5f17cdf2985fd0e723d00a961529422640eead0f
Wellington Dulay <wellington.dulay@gmail.com>
2022-02-15 10:45:19 +1100

    minor tweaks

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -213,9 +213,24 @@ const Navbar: React.FC<{
           className={styles.buttonContainerVertical}
           style={handleVerticalButtons()}
         >
-          <div className={styles.button}>CONTACT</div>
-          <div className={styles.button}>NEWS</div>
-          <div className={styles.button}>STORY</div>
+          <div
+            className={styles.button}
+            style={{ backgroundColor: backgroundColor }}
+          >
+            CONTACT
+          </div>
+          <div
+            className={styles.button}
+            style={{ backgroundColor: backgroundColor }}
+          >
+            NEWS
+          </div>
+          <div
+            className={styles.button}
+            style={{ backgroundColor: backgroundColor }}
+          >
+            STORY
+          </div>
         </div>
       </div>
     </div>
